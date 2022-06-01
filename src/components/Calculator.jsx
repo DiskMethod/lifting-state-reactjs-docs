0f9fe2689a75dcd7291e06d12dc58887631bb708
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-01 16:00:50 -0400

    Finished lifting state tutorial from reactjs docs using hooks instead of class based components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,19 +1,54 @@
 import React, { useState } from "react";
+
+import TemperatureInput from "./TemperatureInput";
 import BoilingVerdict from "./BoilingVerdict";
 
-const Calculator = (props) => {
-  const [temperature, setTemperature] = useState("");
+const toCelsius = (fahrenheit) => {
+  return ((fahrenheit - 32) * 5) / 9;
+};
 
-  const handleChange = (e) => {
-    setTemperature(e.target.value);
+const toFahrenheit = (celsius) => {
+  return (celsius * 9) / 5 + 32;
+};
+
+const tryConvert = (temperature, convert) => {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+};
+
+const Calculator = (props) => {
+  const [celsius, setCelsius] = useState("");
+  const [fahrenheit, setFahrenheit] = useState("");
+
+  const handleOnTemperatureChange = (temperature, scale) => {
+    if (scale === "c") {
+      setCelsius(temperature);
+      setFahrenheit(tryConvert(temperature, toFahrenheit));
+    } else {
+      setFahrenheit(temperature);
+      setCelsius(tryConvert(temperature, toCelsius));
+    }
   };
 
   return (
-    <fieldset>
-      <legend>Enter temperature in Celsius:</legend>
-      <input value={temperature} onChange={handleChange} />
-      <BoilingVerdict celsius={parseFloat(temperature)} />
-    </fieldset>
+    <div>
+      <TemperatureInput
+        scale="c"
+        temperature={celsius}
+        onTemperatureChange={handleOnTemperatureChange}
+      />
+      <TemperatureInput
+        scale="f"
+        temperature={fahrenheit}
+        onTemperatureChange={handleOnTemperatureChange}
+      />
+      <BoilingVerdict celsius={celsius} />
+    </div>
   );
 };
 
